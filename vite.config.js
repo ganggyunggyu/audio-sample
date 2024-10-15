668eb6d6f83990649cdc094394c962e78da3c9be
ganggyunggyu <qwzx16@naver.com>
2024-10-15 13:50:59 +0900

    build config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
-import fs from 'fs';
+// import fs from 'fs';
+// import vue from '@vitejs/plugin-vue';
+
+// const cert = fs.readFileSync('localhost.pem');
+// const key = fs.readFileSync('localhost-key.pem');
+
+// export default {
+//   plugins: [vue()],
+//   resolve: {
+//     alias: [
+//       { find: '@', replacement: '/src' },
+//       { find: '@views', replacement: '/src/views' },
+//       { find: '@resource', replacement: '/src/static/resource' },
+//     ],
+//   },
+//   server: {
+//     host: true,
+//     https: {
+//       key,
+//       cert,
+//     },
+//   },
+// };
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const cert = fs.readFileSync('localhost.pem');
-const key = fs.readFileSync('localhost-key.pem');
-
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@views', replacement: '/src/views' },
-      { find: '@resource', replacement: '/src/static/resource' },
-    ],
-  },
-  server: {
-    host: true,
-    https: {
-      key,
-      cert,
-    },
-  },
-};
+});
